a72b3edbf725c8e7620d96569b83c625e57051b2
harakoji <harakoji@harakoji.net>
2015-09-26 05:15:52 +0000

    fix suggestions test

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -12,55 +12,6 @@ class SuggestionsController < ApplicationController
   def show
   end
 
-  # GET /suggestions/new
-  def new
-    @suggestion = Suggestion.new
-  end
-
-  # GET /suggestions/1/edit
-  def edit
-  end
-
-  # POST /suggestions
-  # POST /suggestions.json
-  def create
-    @suggestion = Suggestion.new(suggestion_params)
-
-    respond_to do |format|
-      if @suggestion.save
-        format.html { redirect_to @suggestion, notice: 'Suggestion was successfully created.' }
-        format.json { render :show, status: :created, location: @suggestion }
-      else
-        format.html { render :new }
-        format.json { render json: @suggestion.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /suggestions/1
-  # PATCH/PUT /suggestions/1.json
-  def update
-    respond_to do |format|
-      if @suggestion.update(suggestion_params)
-        format.html { redirect_to @suggestion, notice: 'Suggestion was successfully updated.' }
-        format.json { render :show, status: :ok, location: @suggestion }
-      else
-        format.html { render :edit }
-        format.json { render json: @suggestion.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /suggestions/1
-  # DELETE /suggestions/1.json
-  def destroy
-    @suggestion.destroy
-    respond_to do |format|
-      format.html { redirect_to suggestions_url, notice: 'Suggestion was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_suggestion
--- test/controllers/suggestions_controller_test.rb
+++ test/controllers/suggestions_controller_test.rb
@@ -11,39 +11,8 @@ class SuggestionsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:suggestions)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create suggestion" do
-    assert_difference('Suggestion.count') do
-      post :create, suggestion: { keyword: @suggestion.keyword }
-    end
-
-    assert_redirected_to suggestion_path(assigns(:suggestion))
-  end
-
   test "should show suggestion" do
     get :show, id: @suggestion
     assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @suggestion
-    assert_response :success
-  end
-
-  test "should update suggestion" do
-    patch :update, id: @suggestion, suggestion: { keyword: @suggestion.keyword }
-    assert_redirected_to suggestion_path(assigns(:suggestion))
-  end
-
-  test "should destroy suggestion" do
-    assert_difference('Suggestion.count', -1) do
-      delete :destroy, id: @suggestion
-    end
-
-    assert_redirected_to suggestions_path
   end
 end
