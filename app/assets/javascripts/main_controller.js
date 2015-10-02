0cf7265a2d1f1a1be53af82621d9db2a20b36941
chihiro hashimoto <chr@chrhsmt.com>
2015-10-02 22:23:32 +0900

    Add sample controllers which access server over http , and views

--- app/assets/javascripts/main_controller.js
+++ app/assets/javascripts/main_controller.js
@@ -1,3 +1,6 @@
-app.controller("MainController", function() {
-  this.user = "john";
-});
+app.controller("MainController", ["$http", function($http) {
+  var vm = this;
+  $http.get("/suggestions/any.json").then(function(res){
+    vm.keyword = res.data.keyword;
+  });
+}]);
--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,7 +1,9 @@
 app.config(function($routeProvider, $locationProvider) {
   $routeProvider
   .when("/", {templateUrl: "partials/main.html", controller: "MainController"})
-  .when("/sample", {templateUrl: "partials/sample.html", controller: "SampleController"})
+  .when("/place", {templateUrl: "partials/place.html", controller: "PlaceController"})
+  .when("/interests", {templateUrl: "partials/interests.html", controller: "InterestsController"})
+  .when("/names", {templateUrl: "partials/names.html", controller: "NamesController"})
   .otherwise({ redirectTo: "/"});
 
   $locationProvider.html5Mode(false);
