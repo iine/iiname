623a345f08d6159022ed445da295ad0066172896
chihiro hashimoto <chr@chrhsmt.com>
2015-10-07 21:17:43 +0900

    Implement temporary view.

--- app/assets/javascripts/names_controller.js
+++ app/assets/javascripts/names_controller.js
@@ -1,2 +1,14 @@
-app.controller("NamesController", function() {
-});
+app.controller("NamesController", ["$http", function($http) {
+  var vm = this;
+  vm.names = [""];
+
+  vm.push = function(e) {
+    console.log(this.names);
+    // $http.get("/").then(function(res) {
+    //   console.log(res);
+    // });
+  }
+  vm.add = function(e) {
+    vm.names.push("");
+  }
+}]);
