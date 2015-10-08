5994499f951c7045a70987d7fa4028dbc9ad65ce
harakoji <harakoji@harakoji.net>
2015-10-08 06:16:26 +0000

    Add locale file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150926041628) do
+ActiveRecord::Schema.define(version: 20151008052200) do
+
+  create_table "statuses", force: :cascade do |t|
+    t.text     "keyword"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "suggestions", force: :cascade do |t|
     t.text     "keyword"
