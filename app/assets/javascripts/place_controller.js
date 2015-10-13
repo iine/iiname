981559a8a47b152d91b8d743241fd4b5b6c277aa
chihiro hashimoto <chr@chrhsmt.com>
2015-10-13 21:11:40 +0900

    Porting view to angularjs.

--- app/assets/javascripts/place_controller.js
+++ app/assets/javascripts/place_controller.js
@@ -1,2 +1,14 @@
-app.controller("PlaceController", function() {
-});
+app.controller("PlaceController", ["$http", "$scope", function($http, $scope) {
+  var vm = this;
+  vm.parent = $scope.$parent;
+  vm.prefectures = [];
+
+  vm.select = function(e) {
+    console.log(e);
+  }
+
+  $http.get("/prefectures.json").then(function(res) {
+    vm.prefectures = res.data;
+  });
+
+}]);
--- app/views/prefectures/index.json.jbuilder
+++ app/views/prefectures/index.json.jbuilder
@@ -1,4 +1,3 @@
 json.array!(@prefectures) do |prefecture|
   json.extract! prefecture, :id, :prefecture, :prefecture_id
-  json.url prefecture_url(prefecture, format: :json)
 end
