ae6d9d57b6ad4bd948f4d5030dc4430641f69f7f
atakei <b1528at@aiit.ac.jp>
2015-10-15 22:31:53 +0900

    Use ng-click

--- app/assets/javascripts/interests_controller.js
+++ app/assets/javascripts/interests_controller.js
@@ -1,6 +1,18 @@
 app.controller("InterestsController", ["$http", "$scope", function($http, $scope) {
   var vm = this;
   vm.parent = $scope.$parent;
+  vm.inames = [""];
 
-  
+  vm.ileft = function() {
+    var results = ['Sea', 'Beach', 'Wave',];
+    var result = Math.floor(Math.random() * results.length);
+    document.getElementById('highest_suggestion').innerHTML = results[result];
+  };
+
+  vm.iright = function() {
+    var results = ['Mountain', 'Green', 'Forest',];
+    var result = Math.floor(Math.random() * results.length);
+    document.getElementById('highest_suggestion').innerHTML = results[result];
+  };
+
 }]);
