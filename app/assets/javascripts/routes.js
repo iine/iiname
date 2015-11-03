ff4da78cfb4cfd612c93230a407112ddf0db38be
chihiro hashimoto <chr@chrhsmt.com>
2015-11-03 14:21:34 +0900

    Fixed double invoking controller

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,9 +1,9 @@
 app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
   $routeProvider
-  .when("/", {templateUrl: "partials/main.html", controller: "MainController"})
-  .when("/place", {templateUrl: "partials/place.html", controller: "PlaceController"})
-  .when("/interests", {templateUrl: "partials/interests.html", controller: "InterestsController"})
-  .when("/names", {templateUrl: "partials/names.html", controller: "NamesController"})
+  .when("/", {templateUrl: "partials/main.html"/*, controller: "MainController"*/})
+  .when("/place", {templateUrl: "partials/place.html"/*, controller: "PlaceController"*/})
+  .when("/interests", {templateUrl: "partials/interests.html"/*, controller: "InterestsController"*/})
+  .when("/names", {templateUrl: "partials/names.html"/*, controller: "NamesController"*/})
   .otherwise({ redirectTo: "/"});
   $locationProvider.html5Mode(false);
 }]);
