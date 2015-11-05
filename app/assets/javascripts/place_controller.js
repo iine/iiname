e407ee673402bfaa1a5b81b85e39ed85ee2fc3a7
chihiro hashimoto <chr@chrhsmt.com>
2015-11-05 22:09:05 +0900

    Remove unnecessary console debugging

--- app/assets/javascripts/place_controller.js
+++ app/assets/javascripts/place_controller.js
@@ -4,15 +4,13 @@ app.controller("PlaceController", ["$http", "$location", "$scope", function($htt
   vm.prefectures = [];
 
   vm.select = function(e) {
-     console.info(vm.prefecture_id);
-
-      　$http.get("/suggestions/any.json", {params: { prefecture_id: vm.prefecture_id}}).then(function(res) {
-           vm.parent.vm.keyword = res.data.keyword;
-           $location.url("/interests");
-      });
+  　$http.get("/suggestions/any.json", {params: { prefecture_id: vm.prefecture_id}}).then(function(res) {
+     vm.parent.vm.keyword = res.data.keyword;
+     $location.url("/interests");
+    });
   }
 
     $http.get("/prefectures.json").then(function(res) {
-        vm.prefectures = res.data;
+      vm.prefectures = res.data;
     });
 }]);
