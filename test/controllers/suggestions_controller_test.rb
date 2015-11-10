e37f6c827dc3e452c105e1470aed1d6d4a23920c
chihiro hashimoto <chr@chrhsmt.com>
2015-11-10 20:52:16 +0900

    Fixed test for morphorogical

--- test/controllers/suggestions_controller_test.rb
+++ test/controllers/suggestions_controller_test.rb
@@ -34,6 +34,6 @@ class SuggestionsControllerTest < ActionController::TestCase
 
   test "should expected keyword" do
     get :show, id: "any", format: :json
-    assert ["花京院", "典明", @title].any? {| word | word == JSON.parse(response.body)["keyword"] }
+    assert ["花京院", "典明", "典明花京院", @title].any? {| word | word == JSON.parse(response.body)["keyword"] }
   end
 end
