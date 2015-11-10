be4fef9b6622797cee1023c05d97791ac51c7cfd
chihiro hashimoto <chr@chrhsmt.com>
2015-11-10 21:40:52 +0900

    Add implementation of interest images for localStorage

--- app/assets/javascripts/interests_controller.js
+++ app/assets/javascripts/interests_controller.js
@@ -1,25 +1,33 @@
-app.controller("InterestsController", ["$http", "$location", "$scope", function($http, $location, $scope) {
+app.controller("InterestsController",
+               ["$http", "$location", "$scope", "$localStorage",
+               function($http, $location, $scope, $localStorage) {
   var vm = this;
   vm.parent = $scope.$parent;
   // vm.inames = [];
   vm.images = [];
 
   vm.ileft = function() {
-    $http.get("/suggestions/any.json", {params: {"interests": vm.images[0].keyword }}).then(function(res){
+    var interests_keyword = vm.images[0].keyword;
+    $localStorage.interests_keywords.push(interests_keyword);
+    $http.get("/suggestions/any.json", {params: {"interests":  interests_keyword}}).then(function(res){
       vm.parent.vm.keyword = res.data.keyword;
       // $location.url("/names");
     });
   };
 
   vm.icenter = function() {
-    $http.get("/suggestions/any.json", {params: {"interests": vm.images[1].keyword }}).then(function(res){
+    var interests_keyword = vm.images[1].keyword;
+    $localStorage.interests_keywords.push(interests_keyword);
+    $http.get("/suggestions/any.json", {params: {"interests": interests_keyword }}).then(function(res){
       vm.parent.vm.keyword = res.data.keyword;
       // $location.url("/names");
     });
   }
 
   vm.iright = function() {
-    $http.get("/suggestions/any.json", {params: {"interests": vm.images[2].keyword }}).then(function(res){
+    var interests_keyword = vm.images[2].keyword;
+    $localStorage.interests_keywords.push(interests_keyword);
+    $http.get("/suggestions/any.json", {params: {"interests": interests_keyword }}).then(function(res){
       vm.parent.vm.keyword = res.data.keyword;
       // $location.url("/names");
     });
