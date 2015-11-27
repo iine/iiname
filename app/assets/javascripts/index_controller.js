d122a58648314c948e5191c8c766c1c16a0c01e3
chihiro hashimoto <chr@chrhsmt.com>
2015-11-27 22:03:16 +0900

    Change first question

--- app/assets/javascripts/index_controller.js
+++ app/assets/javascripts/index_controller.js
@@ -1,5 +1,5 @@
 app.controller("IndexController", ["$scope", "$location", "$http", function($scope, $location, $http) {
   var vm = this;
   vm.parent = $scope.$parent;
-  $location.url("/interests");
+  $location.url("/place");
 }]);
